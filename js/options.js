f36c6b5406c004c50e1a8e6f8e1277446a540130
traceyreinke <traceyreinke@Traceys-MacBook-Pro.local>
2016-07-12 11:54:42 -0500

    upgrading sticky

--- js/options.js
+++ js/options.js
@@ -75,7 +75,15 @@ jQuery(document).ready(function () {
 
     jQuery(function() {
         if(jQuery('.sticky-bar').length) {
-            jQuery(".sticky-bar").sticky({topSpacing:0});       
+            jQuery(".sticky-bar").sticky({topSpacing:0})
+			.on('sticky-start', function () {
+				jQuery('.logo').css('display', 'none');
+				jQuery('.logotext').css('display', '');
+			})
+			.on('sticky-end', function () {
+				jQuery('.logo').css('display', '');
+				jQuery('.logotext').css('display', 'none');
+			});
         }
     });
 
